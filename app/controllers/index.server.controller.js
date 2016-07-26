b6919339800d0e1591c79901c00228a860533928
kangbyungwook <brillante91@naver.com>
2016-07-26 16:04:10 +0900

    display products on index

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,12 +1,20 @@
+var Product = require('mongoose').model('Product');
 exports.render = function(req, res){
   if (req.session.lastVisit){
     console.log(req.session.lastVisit);
   }
 
   req.session.lastVisit = new Date();
+  Product.find({}, function(err, products){
+    if(err){
+      res.send(err);
+    }else{
+      res.render('index',{
+        title: 'Hello world',
+        user: req.user || '',
+        products: products
+      });
+    }
+  })
 
-  res.render('index',{
-    title: 'Hello world',
-    user: req.user || ''
-  });
 };
